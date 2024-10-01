fcbfd8dc8a807950c3f296b3755f2b753f7b5e31
Rasmus Bertell <rasmus@bertell.fi>
2024-10-01 19:41:36 +0300

    feat: use dotfiles home-manager as module

--- flake.nix
+++ flake.nix
@@ -8,16 +8,19 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    dotfiles.url = "github:Rasmus-Bertell/dotfiles";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, dotfiles, ... }@inputs: {
     nixosConfigurations = {
       virvatuli = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         modules = [
           ./config.nix
           ./hosts/virvatuli/config.nix
-          inputs.home-manager.nixosModules.default
+          home-manager.nixosModules.default
+          dotfiles.home.nix
         ];
       };
     };
